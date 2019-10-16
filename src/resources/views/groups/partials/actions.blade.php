5be053ac881aafbb2d09ef19eae8e0f7ebf38ed3
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2019-10-16 18:25:00 +0200

    fix(deps): upgrade notifications groups list

--- src/resources/views/groups/partials/actions.blade.php
+++ src/resources/views/groups/partials/actions.blade.php
@@ -1,10 +1,12 @@
-<div class="btn-group pull-right">
+<div class="btn-group btn-group-sm float-right">
   <a href="{{ route('notifications.groups.edit', ['notification_group_id' => $row->id]) }}"
-     type="button" class="btn btn-primary btn-xs">
+     type="button" class="btn btn-warning btn-xs">
+    <i class="fas fa-pencil-alt"></i>
     {{ trans('web::seat.edit') }}
   </a>
   <a href="{{ route('notifications.groups.delete', ['group_id' => $row->id]) }}"
      type="button" class="btn btn-danger btn-xs confirmlink">
+    <i class="fas fa-trash-alt"></i>
     {{ trans('web::seat.delete') }}
   </a>
 </div>
