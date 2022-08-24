145ce31ba1f1a716cdedd97b1edfe60607421889
Leon Jacobs <leonja511@gmail.com>
2022-08-24 15:38:58 +0200

    New translations alerts.php (Romanian)

---
+++ src/resources/lang/ro/alerts.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * This file is part of SeAT
+ *
+ * Copyright (C) 2015 to 2022 Leon Jacobs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+return [
+    // Core
+    'created_user'                     => 'Utilizator creat',
+
+    //Squads
+    'squad_application'                => 'Aplicațiile echipei',
+    'squad_member'                     => 'Membru nou în echipă',
+    'squad_member_removed'             => 'Membru eliminat al echipei',
+
+    // Esi
+    'alliance_anchoring'               => 'Alliance Anchoring Report',
+    'alliance_war_declared'            => 'Declared War against Ally',
+    'alliance_war_invalidated'         => 'Invalidated War against Ally',
+    'alliance_capital_changed'         => 'Changed Capital Alliance',
+    'ally_joined_war_aggressor'        => 'Aggressor Ally Joined War',
+    'ally_joined_war_ally'             => 'Ally Joined War',
+    'ally_joined_war_defender'         => 'Defender Ally Joined War',
+    'bill_paid_corporation_alliance'   => 'Corporation Paid Bill',
+    'character_left_corporation'       => 'Corporation Leaving Character',
+    'corporation_alliance_bill'        => 'New Alliance Bill',
+    'corporation_application_new'      => 'New Corporation Application',
+    'moon_mining_extraction_finished'  => 'MM Finished Extractions',
+    'moon_mining_extraction_started'   => 'MM Started Extractions',
+    'orbital_attacked'                 => 'Attacked Customs Office',
+    'ownership_transferred'            => 'Transferred Ownership',
+    'raffle_created'                   => 'Created Raffle',
+    'raffle_expired'                   => 'Expired Raffle',
+    'raffle_finished'                  => 'Finished Raffle',
+    'research_mission_available'       => 'New Research Mission',
+    'sovereignty_structure_destroyed'  => 'Destroyed Sovereignty Structure',
+    'sovereignty_structure_reinforced' => 'Reinforced Sovereignty Structure',
+    'story_line_mission_available'     => 'New Story Line',
+    'structure_anchoring'              => 'Anchoring Structures',
+    'structure_destroyed'              => 'Destroyed Structures',
+    'structure_fuel_alert'             => 'Low Fuel Structures',
+    'structure_lost_armor'             => 'Lost Armor Structures',
+    'structure_lost_shield'            => 'Lost Shield Structures',
+    'structure_services_offline'       => 'Structures Offline Services',
+    'structure_unanchoring'            => 'Unanchoring Structures',
+    'structure_under_attack'           => 'Attacked Structures',
+    'structure_went_high_power'        => 'Structures High Power',
+    'structure_went_low_power'         => 'Structures Low Power',
+    'killmails'                        => 'Killmail-uri',
+];
