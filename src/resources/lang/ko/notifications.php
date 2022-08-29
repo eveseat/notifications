bc1ca0a56b342630849e8569563cf501781c5477
Leon Jacobs <leonja511@gmail.com>
2022-08-29 07:49:27 +0200

    New translations notifications.php (Korean)

--- src/resources/lang/ko/notifications.php
+++ src/resources/lang/ko/notifications.php
@@ -22,21 +22,21 @@
 
 return [
 
-    'group'      => 'Notification Group|Notification Groups',
-    'new_group'  => 'New Notifications Group',
-    'edit_group' => 'Edit Group',
-    'group_name' => 'Group Name',
-    'group_type' => 'Group Type',
+    'group'      => '알림그룹|알림그룹',
+    'new_group'  => '새로운 알림 그룹',
+    'edit_group' => '그룹 편집',
+    'group_name' => '그룹 이름',
+    'group_type' => '그룹 타입',
 
-    'add'            => 'Add',
-    'name'           => 'Name',
-    'type'           => 'Type',
-    'alert'          => 'Alert|Alerts',
-    'integration'    => 'Integration|Integrations',
-    'affiliation'    => 'Affiliation|Affiliations',
-    'add_all_alerts' => 'Add All Alerts',
+    'add'            => '추가',
+    'name'           => '이름',
+    'type'           => '타입',
+    'alert'          => '경고|경고',
+    'integration'    => '통합|통합',
+    'affiliation'    => '제휴|제휴',
+    'add_all_alerts' => '모든 경고 추가',
 
-    'no_affiliation_notice' => 'No affiliations have been assosciated with this group ' .
+    'no_affiliation_notice' => '이 그룹에 연결된 소속이 없습니다' .
         'yet. In this configuration, all notifications for the matched alerts will be ' .
         'sent without any filter.',
 
