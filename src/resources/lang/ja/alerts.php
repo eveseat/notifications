e56e4d80827ffa718db848240b231d8ae7657d1b
Leon Jacobs <leonja511@gmail.com>
2022-11-24 23:45:09 +0200

    lang: add japanese support (#634)
    * New translations alerts.php (Japanese)
    * New translations notifications.php (Japanese)
    * New translations alerts.php (Japanese)
    * New translations notifications.php (Japanese)

---
+++ src/resources/lang/ja/alerts.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * This file is part of SeAT
+ *
+ * Copyright (C) 2015 to 2022 Leon Jacobs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+return [
+    // Core
+    'created_user'                     => '作成されたユーザー',
+
+    //Squads
+    'squad_application'                => 'スクワッドアプリケーション',
+    'squad_member'                     => 'スクワッドの新しいメンバー',
+    'squad_member_removed'             => 'スクワッドから除外されたメンバー',
+
+    // Esi
+    'alliance_anchoring'               => 'アライアンスの係留レポート',
+    'alliance_war_declared'            => 'アライアンスに対して宣言された戦争',
+    'alliance_war_invalidated'         => 'アライアンスに対して終了した戦争',
+    'alliance_capital_changed'         => '中核アライアンスの変更',
+    'ally_joined_war_aggressor'        => '戦争に参加している攻撃側アライアンス',
+    'ally_joined_war_ally'             => '戦争に参加しているアライアンス',
+    'ally_joined_war_defender'         => '戦争に参加している防衛側アライアンス',
+    'bill_paid_corporation_alliance'   => 'コーポレーションの支払い済み請求書',
+    'character_left_corporation'       => 'コーポレーションを脱退したキャラクター',
+    'corporation_alliance_bill'        => '新しいアライアンス請求',
+    'corporation_application_new'      => '新しいコーポレーションアプリケーション',
+    'moon_mining_extraction_finished'  => '月掘りが終了しました',
+    'moon_mining_extraction_started'   => '月掘りを開始しました',
+    'orbital_attacked'                 => '攻撃された税関',
+    'ownership_transferred'            => 'オーナー権限の委譲',
+    'raffle_created'                   => '作成された抽選',
+    'raffle_expired'                   => '期限切れの抽選',
+    'raffle_finished'                  => '終了した抽選',
+    'research_mission_available'       => '新しい研究ミッション',
+    'sovereignty_structure_destroyed'  => '破壊された所有ストラクチャ',
+    'sovereignty_structure_reinforced' => '強化状態の所有ストラクチャ',
+    'story_line_mission_available'     => '新しいストーリーライン',
+    'structure_anchoring'              => '係留中のストラクチャ',
+    'structure_destroyed'              => '破壊されたストラクチャ',
+    'structure_fuel_alert'             => '燃料切れ寸前のストラクチャ',
+    'structure_lost_armor'             => 'アーマー全損ストラクチャ',
+    'structure_lost_shield'            => 'シールド全損ストラクチャ',
+    'structure_services_offline'       => 'ストラクチャのオフラインサービス',
+    'structure_unanchoring'            => '係留解除中のストラクチャ',
+    'structure_under_attack'           => '攻撃されたストラクチャ',
+    'structure_went_high_power'        => 'オンライン状態のストラクチャ',
+    'structure_went_low_power'         => 'ローパワー状態のストラクチャ',
+    'killmails'                        => 'キルメール',
+];
---
+++ src/resources/lang/ja/notifications.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * This file is part of SeAT
+ *
+ * Copyright (C) 2015 to 2022 Leon Jacobs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+return [
+
+    'group'      => '通知グループ',
+    'new_group'  => '新たな通知グループ',
+    'edit_group' => 'グループの編集',
+    'group_name' => 'グループ名',
+    'group_type' => 'グループタイプ',
+
+    'add'            => '追加',
+    'name'           => '名前',
+    'type'           => 'タイプ',
+    'alert'          => 'アラート',
+    'integration'    => '外部サービス連携 | 一覧',
+    'affiliation'    => '加入者 | 一覧',
+    'add_all_alerts' => 'すべてのアラートを追加',
+
+    'no_affiliation_notice' => 'このグループに紐づけられている加入者は ' .
+        'まだいません. この設定ではマッチしたアラートによる全ての通知が ' .
+        'フィルターなしで送信されます。',
+
+    'new_integration'         => '新規外部サービス連携',
+    'new_integration_message' => '新規外部サービス連携',
+    'new_email'               => '新たなE-メールの統合',
+    'new_slack'               => '新たなslackの統合',
+    'configured_integrations' => '外部サービスの連携設定',
+    'settings'                => '設定',
+
+    'setup_label'             => '通知の設定',
+    'setup_description'       => 'ユーザーが通知チャンネルとサブスクリプションを定義できるようにします。',
+];
