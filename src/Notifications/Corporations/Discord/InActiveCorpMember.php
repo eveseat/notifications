030a16d2127c259750b4bb9801f0c8f31025906d
Loïc Leuilliot <loic.leuilliot@gmail.com>
2023-07-27 19:46:56 +0200

    style: update copyright header (#85)
    Co-authored-by: StyleCI Bot <bot@styleci.io>

--- src/Notifications/Corporations/Discord/InActiveCorpMember.php
+++ src/Notifications/Corporations/Discord/InActiveCorpMember.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of SeAT
  *
- * Copyright (C) 2015 to 2022 Leon Jacobs
+ * Copyright (C) 2015 to present Leon Jacobs
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -30,7 +30,7 @@ use Seat\Notifications\Services\Discord\Messages\DiscordMessage;
 class InActiveCorpMember extends AbstractDiscordNotification
 {
      /**
-      * @var \Seat\Eveapi\Models\Corporation\CorporationMemberTracking
+       * @var \Seat\Eveapi\Models\Corporation\CorporationMemberTracking
       */
      private $member;
 
