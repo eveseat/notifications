656cb70e1397c91f140c7283ecea58f64d2d60d4
recursive_tree <recursivetreemail@gmail.com>
2023-10-24 13:45:04 +0200

    styleci

--- src/Observers/KillmailNotificationObserver.php
+++ src/Observers/KillmailNotificationObserver.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of SeAT
  *
- * Copyright (C) 2015 to 2022 Leon Jacobs
+ * Copyright (C) 2015 to present Leon Jacobs
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -22,10 +22,8 @@
 
 namespace Seat\Notifications\Observers;
 
-use Illuminate\Notifications\AnonymousNotifiable;
 use Seat\Eveapi\Models\Killmails\KillmailDetail;
 use Seat\Notifications\Models\NotificationGroup;
-use Seat\Notifications\Notifications\Characters\Slack\Killmail;
 use Seat\Notifications\Traits\NotificationDispatchTool;
 
 /**
