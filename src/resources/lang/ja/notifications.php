19022670bedc5424c6c9f8fbfb51c4d81891ea2e
Leon Jacobs <leonja511@gmail.com>
2024-10-02 15:16:36 +0200

    New translations notifications.php (Japanese)

--- src/resources/lang/ja/notifications.php
+++ src/resources/lang/ja/notifications.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of SeAT
  *
- * Copyright (C) 2015 to 2022 Leon Jacobs
+ * Copyright (C) 2015 to present Leon Jacobs
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -22,31 +22,46 @@
 
 return [
 
-    'group'      => '通知グループ',
-    'new_group'  => '新たな通知グループ',
+    'group' => '通知グループ',
+    'new_group' => '新たな通知グループ',
     'edit_group' => 'グループの編集',
     'group_name' => 'グループ名',
     'group_type' => 'グループタイプ',
 
-    'add'            => '追加',
-    'name'           => '名前',
-    'type'           => 'タイプ',
-    'alert'          => 'アラート',
-    'integration'    => '外部サービス連携 | 一覧',
-    'affiliation'    => '加入者 | 一覧',
+    'add' => '追加',
+    'name' => '名前',
+    'type' => 'タイプ',
+    'alert' => 'アラート',
+    'integration' => '外部サービス連携 | 一覧',
+    'affiliation' => '加入者 | 一覧',
     'add_all_alerts' => 'すべてのアラートを追加',
 
     'no_affiliation_notice' => 'このグループに紐づけられている加入者は ' .
         'まだいません. この設定ではマッチしたアラートによる全ての通知が ' .
         'フィルターなしで送信されます。',
 
-    'new_integration'         => '新規外部サービス連携',
+    'new_integration' => '新規外部サービス連携',
     'new_integration_message' => '新規外部サービス連携',
-    'new_email'               => '新たなE-メールの統合',
-    'new_slack'               => '新たなslackの統合',
+    'new_discord' => 'New Discord Integration',
+    'new_email' => '新たなE-メールの統合',
+    'new_slack' => '新たなslackの統合',
     'configured_integrations' => '外部サービスの連携設定',
-    'settings'                => '設定',
+    'settings' => '設定',
 
-    'setup_label'             => '通知の設定',
-    'setup_description'       => 'ユーザーが通知チャンネルとサブスクリプションを定義できるようにします。',
+    'setup_label' => '通知の設定',
+    'setup_description' => 'ユーザーが通知チャンネルとサブスクリプションを定義できるようにします。',
+
+    'test_integration' => 'Test',
+
+    'mention' => 'Mention|Mentions',
+    'data' => 'Data',
+    'actions' => 'Actions',
+    'successfully_created_mention' => 'Successfully created a new mention!',
+    'create_mention' => 'Create Mention',
+    'create_discord_role_mention' => 'New Discord Role Mention',
+    'discord_role_id' => 'Discord Role ID',
+    'discord_role_id_help' => 'Enter the ID of the discord role to ping. Make sure to enable discord\'s developer mode in the \'Advanced\' section of your settings. Afterwards, open the server settings, open the \'Roles\' section, click the role and select \'Copy Role ID\'.',
+    'create_discord_user_mention' => 'New Discord User Mention',
+    'discord_user_id' => 'Discord User ID',
+    'discord_user_id_help' => 'Enter the ID of the discord user to ping. Make sure to enable discord\'s developer mode in the \'Advanced\' section of your settings. Afterwards, click on the avatar in the member list and select \'Copy User ID\'.',
 ];
