ffb24456cc5df35f79c00cccee7180363d1879ca
Leon Jacobs <leonja511@gmail.com>
2024-10-02 15:16:25 +0200

    New translations notifications.php (Korean)

--- src/resources/lang/ko/notifications.php
+++ src/resources/lang/ko/notifications.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of SeAT
  *
- * Copyright (C) 2015 to 2022 Leon Jacobs
+ * Copyright (C) 2015 to present Leon Jacobs
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -22,31 +22,46 @@
 
 return [
 
-    'group'      => '알림그룹|알림그룹',
-    'new_group'  => '새로운 알림 그룹',
+    'group' => '알림그룹|알림그룹',
+    'new_group' => '새로운 알림 그룹',
     'edit_group' => '그룹 편집',
     'group_name' => '그룹 이름',
     'group_type' => '그룹 타입',
 
-    'add'            => '추가',
-    'name'           => '이름',
-    'type'           => '타입',
-    'alert'          => '경고|경고',
-    'integration'    => '통합|통합',
-    'affiliation'    => '제휴|제휴',
+    'add' => '추가',
+    'name' => '이름',
+    'type' => '타입',
+    'alert' => '경고|경고',
+    'integration' => '통합|통합',
+    'affiliation' => '제휴|제휴',
     'add_all_alerts' => '모든 경고 추가',
 
     'no_affiliation_notice' => '이 그룹에 연결된 소속이 없습니다' .
         'yet. In this configuration, all notifications for the matched alerts will be ' .
         'sent without any filter.',
 
-    'new_integration'         => 'New Integration',
+    'new_integration' => 'New Integration',
     'new_integration_message' => 'Add a New Integration',
-    'new_email'               => '이메일 신규 연동',
-    'new_slack'               => 'Slack 신규 연동',
+    'new_discord' => 'New Discord Integration',
+    'new_email' => '이메일 신규 연동',
+    'new_slack' => 'Slack 신규 연동',
     'configured_integrations' => '환경설정 연동',
-    'settings'                => 'Settings',
+    'settings' => 'Settings',
 
-    'setup_label'             => 'Notifications Setup',
-    'setup_description'       => 'Allow user to define notification channels and subscriptions.',
+    'setup_label' => 'Notifications Setup',
+    'setup_description' => 'Allow user to define notification channels and subscriptions.',
+
+    'test_integration' => 'Test',
+
+    'mention' => 'Mention|Mentions',
+    'data' => 'Data',
+    'actions' => 'Actions',
+    'successfully_created_mention' => 'Successfully created a new mention!',
+    'create_mention' => 'Create Mention',
+    'create_discord_role_mention' => 'New Discord Role Mention',
+    'discord_role_id' => 'Discord Role ID',
+    'discord_role_id_help' => 'Enter the ID of the discord role to ping. Make sure to enable discord\'s developer mode in the \'Advanced\' section of your settings. Afterwards, open the server settings, open the \'Roles\' section, click the role and select \'Copy Role ID\'.',
+    'create_discord_user_mention' => 'New Discord User Mention',
+    'discord_user_id' => 'Discord User ID',
+    'discord_user_id_help' => 'Enter the ID of the discord user to ping. Make sure to enable discord\'s developer mode in the \'Advanced\' section of your settings. Afterwards, click on the avatar in the member list and select \'Copy User ID\'.',
 ];
