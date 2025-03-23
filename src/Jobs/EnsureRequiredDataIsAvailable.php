3f051ce4113629c4860f8d1b7f5b68d72e36cf98
tmas <tmas@tgaragemedia.com>
2025-03-23 10:37:39 -0400

    More updates to comply with styleci :)

--- src/Jobs/EnsureRequiredDataIsAvailable.php
+++ src/Jobs/EnsureRequiredDataIsAvailable.php
@@ -23,7 +23,6 @@
 namespace Seat\Notifications\Jobs;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
