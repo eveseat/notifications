4ca9fc9b61ffc0c0e33f8feda28a0310ec133a1c
Leon Jacobs <leonja511@gmail.com>
2025-11-16 22:28:58 +0200

    New translations notifications.php (Japanese)

--- src/resources/lang/ja/notifications.php
+++ src/resources/lang/ja/notifications.php
@@ -42,6 +42,7 @@ return [
 
     'new_integration' => '新規外部サービス連携',
     'new_integration_message' => '新規外部サービス連携',
+    'new_discord' => 'New Discord Integration',
     'new_email' => '新たなE-メールの統合',
     'new_slack' => '新たなslackの統合',
     'configured_integrations' => '外部サービスの連携設定',
@@ -49,4 +50,18 @@ return [
 
     'setup_label' => '通知の設定',
     'setup_description' => 'ユーザーが通知チャンネルとサブスクリプションを定義できるようにします。',
+
+    'test_integration' => 'Test',
+
+    'mention' => 'Mention|Mentions',
+    'data' => 'Data',
+    'actions' => 'Actions',
+    'successfully_created_mention' => 'Successfully created a new mention!',
+    'create_mention' => 'Create Mention',
+    'create_discord_role_mention' => 'New Discord Role Mention',
+    'discord_role_id' => 'Discord Role ID',
+    'discord_role_id_help' => 'Enter the ID of the discord role to ping. Make sure to enable discord\'s developer mode in the \'Advanced\' section of your settings. Afterwards, open the server settings, open the \'Roles\' section, click the role and select \'Copy Role ID\'.',
+    'create_discord_user_mention' => 'New Discord User Mention',
+    'discord_user_id' => 'Discord User ID',
+    'discord_user_id_help' => 'Enter the ID of the discord user to ping. Make sure to enable discord\'s developer mode in the \'Advanced\' section of your settings. Afterwards, click on the avatar in the member list and select \'Copy User ID\'.',
 ];
